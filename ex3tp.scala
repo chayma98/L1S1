4fb7141bcda53ca00f43c458b2cc0ae0d477219b
chayma98 <43567381+chayma98@users.noreply.github.com>
2018-11-13 22:08:40 +0100

    Add files via upload

---
+++ ex3tp.scala
@@ -0,0 +1,10 @@
+
+
+object ex3tp extends App {
+   import scala.io.StdIn ; 
+  object exemple_if extends App {
+    var i , n : Int = 0 ;
+    print (" donner un nombre");
+    for ( i = 1 to n) { 
+      if ( n % i != 0 ) { print (n ," n'est pas premier " )} 
+    }
