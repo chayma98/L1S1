05930ecbb51affa501526ec6621f899fb7246299
chayma98 <43567381+chayma98@users.noreply.github.com>
2018-11-13 21:57:26 +0100

    Add files via upload

---
+++ ex2tp.scala
@@ -0,0 +1,10 @@
+
+
+object ex2tp extends App {
+  import scala.io.StdIn ; 
+  object exemple_if extends App { 
+    var i ,n , s : Int = 0 
+    s = 1 ;
+    for (i = 1 to n ) { s = s*i }
+    print (s) ;
+  }
