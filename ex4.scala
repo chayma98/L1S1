874b62c7e7983c80ad85cf736525d6ac8cbd3ada
chayma98 <43567381+chayma98@users.noreply.github.com>
2018-11-13 22:24:46 +0100

    Add files via upload

---
+++ ex4.scala
@@ -0,0 +1,9 @@
+
+import scala.io.StdIn ;
+object ex4 extends App {
+  var n ,s , i : Int = 0 ;
+  for (i = 1 to n ) { if (n % i == 0 ) { s = s + i } } 
+   if ( s == 2*n ) { print ( n " est un nbre parfait " ) }
+}
+  
+  
